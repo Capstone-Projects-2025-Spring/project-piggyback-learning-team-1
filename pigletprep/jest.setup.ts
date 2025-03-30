bd180cfa8b7a52484a413b30f91f713a9dd3a7df
Noel Chacko <noelchacko@example.com>
2025-03-30 16:13:23 -0400

    make tests on homepage more in-depth

--- pigletprep/jest.setup.ts
+++ pigletprep/jest.setup.ts
@@ -1,1 +1,8 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+
+// Add TextEncoder to the global scope
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util')
+  global.TextEncoder = TextEncoder
+  global.TextDecoder = TextDecoder
+}
